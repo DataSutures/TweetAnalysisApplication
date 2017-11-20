e3f5efb2cece5e0e2fdf7c5a54c78ec41c90e4a4
Kimberly Smith <datasutures@gmail.com>
2017-11-20 15:50:52 -0600

    adding geoCode filter properly, according to twitter4j standards

--- src/main/java/com/mycompany/mavenproject1/TwitterQuery.java
+++ src/main/java/com/mycompany/mavenproject1/TwitterQuery.java
@@ -7,6 +7,7 @@ package com.mycompany.mavenproject1;
 
 import java.util.ArrayList;
 import java.util.List;
+import twitter4j.GeoLocation;
 
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -23,8 +24,9 @@ public class TwitterQuery {
         Twitter twitter = new TwitterFactory().getInstance();
         List<Status> tweetsList = new ArrayList<>();
         try {
-            String filters = " -filter:links -filter:retweets -filter:replies geocode:37.09024,-95.712891,2500km";
+            String filters = " -filter:links -filter:retweets -filter:replies";
             Query q = new Query(topic + filters);
+            q.geoCode(new GeoLocation(37.09024,-95.712891), 2500, Query.KILOMETERS.toString());
             q.setCount(10);
             q.setLang("en");
             QueryResult r = twitter.search(q);  
